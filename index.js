01500d641465c3b555638b0b72b9a05ca1252d55
ikbakkk <iqbalfirdaus05@gmail.com>
2022-12-15 08:57:15 +0800

    3.7 add morgan

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 let persons = [
   {
@@ -25,6 +26,7 @@ let persons = [
 ];
 
 app.use(express.json());
+app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
