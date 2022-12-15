c6cfd461fd16e651e0c7eb689c64d0f4fcffe1dc
ikbakkk <iqbalfirdaus05@gmail.com>
2022-12-15 09:22:46 +0800

    port change

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ app.get("/info", (request, response) => {
   response.send;
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
